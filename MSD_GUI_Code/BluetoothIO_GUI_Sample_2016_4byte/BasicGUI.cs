103b33f23993fa58fade1259fab4be5caa558c64
L-Warlok <lukewarlik@kolabnow.com>
2018-05-04 10:13:37 +0800

    Rewrite new_master2 with master

--- MSD_GUI_Code/BluetoothIO_GUI_Sample_2016_4byte/BasicGUI.cs
+++ MSD_GUI_Code/BluetoothIO_GUI_Sample_2016_4byte/BasicGUI.cs
@@ -10,20 +10,16 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using System.Diagnostics;
 using System.Threading;
-
 
 namespace BluetoothGUISample
 {    
 
-    
 
     public partial class Form1 : Form
     {
         // Declare variables to store inputs and outputs.
-
-        bool runBluetooth = false;
+        bool runBluetooth = true;
         int Input1 = 0;
         int Input2 = 0;
         int DecTickNew = 0;
@@ -31,9 +27,15 @@ namespace BluetoothGUISample
         double TotalTicks = 0;
         int Diff = 0;
         double time = 0;
+        double error = 0;
         int BitVal1 = 0;
         int Lowbyte = 0;
         int Highbyte = 0;
+
+        Thread thrtb1;
+        Thread thrtb2;
+        Thread thrtb3;
+
 
         // Circumference of Decoder DIsc
         // 
@@ -44,52 +46,13 @@ namespace BluetoothGUISample
         const byte START = 255;
         const byte ZERO = 0;
 
-        int controlMode = 0; // 0-no control, 1-open loop, 2-closed loop
+        private BackgroundWorker myWorker = new BackgroundWorker();
 
-        const int noControl = 0;
-        const int openLoop = 1;
-        const int closedLoop = 2;
-
-        int PIDMode = 0;
-        const int position = 0;
-        const int velocity = 1;
-        const int acceleration = 2;
-
-        // PID variables
-        float posSet = 0;
-        float velSet = 0;
-        float accSet = 0;
-        float setpoint = 0;
-
-        int error = 0;
-        int iError = 0;
-        int dError = 0;
-        int prevError = 0;
-
-        float Kp = 0;
-        float Ki = 0;
-        float Kd = 0;
-
-        byte controlAction = 0;
-        byte OLSpeed = 0;
 
         public Form1()
         {
-
-
             // Initialize required for form controls.
             InitializeComponent();
-
-
-            Stopwatch watch = new Stopwatch();
-            watch.Start();
-
-           // Thread t = new Thread(() => outputTimer(watch, textBox3));          // Kick off a new thread
-            //t.IsBackground = true;
-           // t.Start();                               // running WriteY()
- 
-
-
 
             // Establish connection with Bluetooth IOCard
             if (runBluetooth == true)
@@ -106,20 +69,16 @@ namespace BluetoothGUISample
                     }
                 }
             }
-        }
-        /*
-        private void outputTimer (Stopwatch watch, TextBox textBox)
-        {
+            //thrtb1 = new Thread(Tb1ControlsUpdate);
+            //thrtb1.Start();
 
-            while(true)
-            {
-                textBox.Invalidate();
-                textBox.Text = watch.ElapsedMilliseconds.ToString();
-                
-               // textBox.Update();
-            }
+            Thread test = new Thread(new ThreadStart(TestThread));
+            test.Start();
+
+
+
         }
-        */
+
         // Send a four byte message to the Arduino via serial.
         private void SendIO(byte PORT, byte DATA)
         {
@@ -134,6 +93,8 @@ namespace BluetoothGUISample
             }
         }
 
+
+
         private void button2_Click(object sender, EventArgs e) //Press the button to send the value to Output 1, Arduino Port A.
         {
              SendIO(0, 0);
@@ -142,42 +103,44 @@ namespace BluetoothGUISample
                 if (BitValue1.Value < 150 & BitValue1.Value > 127)
                 {
                     BitVal1 = 150;
-                    SendIO(2, (byte)BitVal1); // The value 1 indicates troubleshooting box 1.}
+                    SendIO(1, (byte)BitVal1); // The value 1 indicates troubleshooting box 1.}
                 }
                 else if (BitValue1.Value < 127 & BitValue1.Value > 107)
                 {
                     BitVal1 = 107;
-                    SendIO(2, (byte)BitVal1); // The value 1 indicates troubleshooting box 1.}
+                    SendIO(1, (byte)BitVal1); // The value 1 indicates troubleshooting box 1.}
                 }
                 
                 else 
                 {
-                    SendIO(2, (byte)BitValue1.Value); // The value 1 indicates troubleshooting box 1.
+                    SendIO(1, (byte)BitValue1.Value); // The value 1 indicates troubleshooting box 1.
                 }
             }
 
             else if (DeadbandBox.Checked == false)
             {
-                SendIO(2, (byte)BitValue1.Value); // The value 1 indicates troubleshooting box 1.}
+                SendIO(1, (byte)BitValue1.Value); // The value 1 indicates troubleshooting box 1.}
             }
+
+
 
         }
         // Distance Changer Button
         private void PosButton_Click(object sender, EventArgs e) //Press the button to send the value to Output 1, Arduino Port A.
         {
-            SendIO(2, (byte)NextPos.Value); // The value 1 indicates troubleshooting box 1.
+            SendIO(1, (byte)NextPos.Value); // The value 1 indicates troubleshooting box 1.
         }
 
         // Velocity Changer Button
         private void VelButton_Click(object sender, EventArgs e) //Press the button to send the value to Output 1, Arduino Port A.
         {
-            SendIO(2, (byte)NextVel.Value); // The value 1 indicates troubleshooting box 1.
+            SendIO(1, (byte)NextVel.Value); // The value 1 indicates troubleshooting box 1.
         }
 
         // Acceleration Changer Button
         private void AccButton_Click(object sender, EventArgs e) //Press the button to send the value to Output 1, Arduino Port A.
         {
-            SendIO(2, (byte)NextAcc.Value); // The value 1 indicates troubleshooting box 1.
+            SendIO(1, (byte)NextAcc.Value); // The value 1 indicates troubleshooting box 1.
         }
 
 
@@ -195,11 +158,11 @@ namespace BluetoothGUISample
         {
             if (bluetooth.IsOpen) //Check that a serial connection exists.
             {
-
+                
 
                 // READ DATA FROM THE ARDUINO
                 //-----------------------------------------------------------------------------------------------------------
-                if (bluetooth.BytesToRead >= 5) //Check that the buffer contains a full four byte package.
+                if (bluetooth.BytesToRead >= 4) //Check that the buffer contains a full four byte package.
                 {
                     Inputs[0] = (byte)bluetooth.ReadByte(); //Read the first byte of the package.
 
@@ -208,59 +171,45 @@ namespace BluetoothGUISample
                         time++;
 
                         //Read the rest of the package.
-                        Inputs[1] = (byte)bluetooth.ReadByte();
+                        Inputs[1] = (byte)bluetooth.ReadByte(); // Data byte (The byte thats useful)
                         Inputs[2] = (byte)bluetooth.ReadByte(); // Data byte (The byte thats useful)
-                        Inputs[3] = (byte)bluetooth.ReadByte(); // Data byte (The byte thats useful)
-                        Inputs[4] = (byte)bluetooth.ReadByte();
-
-                        Lowbyte = Inputs[2];
-                        Highbyte = Inputs[3];
-
-                        DecTickNew = Lowbyte + 256*Highbyte;
-
-
-                        Diff = DecTickNew - DecTickOld;
-
-
-                        if (Diff > 30000)
-                        { Diff = 1; }
-
-                        else if (Diff < -30000)
-                        { Diff = -1; }
-
-                        DecTickOld = DecTickNew;
-                        TotalTicks += Diff;
-                        TotalTicksBox.Text = TotalTicks.ToString();
-                        TotalTicksBox.Update(); // Hopefully updates textbox
-                        TotalTicksLabel.Text = TotalTicks.ToString(); // If this works im going to cry
-
-                        //Print to Position Graph
-                        textBox4.Text = (Math.Abs(0.039885 * TotalTicks) + Math.Sin(2)).ToString();
-                        PositionGraph.Series[0].Points.AddXY(time, PositionGraphCalc(TotalTicks));
-                        PositionGraph.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                        Inputs[3] = (byte)bluetooth.ReadByte(); 
 
                         //Calculate the checksum.
                         byte checkSum = (byte)(Inputs[0] + Inputs[1] + Inputs[2]);
 
-                        //Check that the calculated check sum matches the checksum sent with the message.
-                        if (Inputs[4] == checkSum)
+                        if (Inputs[3] == checkSum)
                         {
-                            //Check which port the incoming data is associated with.
-                            switch (Inputs[1])
-                            {
+                            Lowbyte = Inputs[1];
+                            Highbyte = Inputs[2];
 
-                                case 0: //Save the data to a variable and place in the textbox.
-                                    
+                            DecTickNew = Lowbyte + 256*Highbyte;
+                            Console.WriteLine(DecTickNew.ToString());
 
- 
 
-                                    TotalTicksBox.Text = TotalTicks.ToString();
-                                    break;
-                                case 1: //Save the data to a variable and place in the textbox. 
-                                    Input2 = Inputs[2];
-                                    break;
-                            }
+                            Diff = DecTickNew - DecTickOld;
+
+
+                            //if (Diff > 30000)
+                            //{ Diff = 1; }
+
+                            //else if (Diff < -30000)
+                            //{ Diff = -1; }
+
+                            DecTickOld = DecTickNew;
+                            TotalTicks += Diff;
+                            //TotalTicksBox.Text = TotalTicks.ToString();
+                            //TotalTicksBox.Update(); // Hopefully updates textbox
+                            //TotalTicksLabel.Text = TotalTicks.ToString(); // If this works im going to cry
+
+                            //Print to Position Graph
+                            //textBox4.Text = (Math.Abs(0.039885 * TotalTicks) + Math.Sin(2)).ToString();
+                            //PositionGraph.Series[0].Points.AddXY(time, PositionGraphCalc(TotalTicks));
+                            //PositionGraph.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+
+
                         }
+
                     }
                 }
 
@@ -275,57 +224,17 @@ namespace BluetoothGUISample
                 // 
 
 
-                
-                switch (controlMode)
-                {
-                    case noControl:
-                        controlAction = 127;
-                    case openLoop:
-                        controlAction = OLSpeed;
-                        break;
-                    case closedLoop:
-                        switch (PIDMode)
-                        {
-                            case position:
-                                setpoint = posSet;
-                                //error = setpoint - currentCountVal
-                                break;
-
-                            case velocity:
-                                //setpoint = box;
-                                //current = DecTickNew;
-                                break;
-
-                            case acceleration:
-                                break;
+                //
+                //
+                //
+                //-----------------------------------------------------------------------------------------------------------
 
 
-  
-                        }
-                        // General PID error calcs
-                        //error = setpoint - current;
-                        iError = prevError + error;
-                        dError = error - prevError;
-                        // 
-                        prevError = error;
-                        //
-                        //
-                        controlAction = (byte)(Kp * error + Ki * iError + Kd * dError);
-                        
-
-                        break;
-                    default:
-                        break;
-
-                }
-                SendIO(1, controlAction);
+                //-----------------------------------------------------------------------------------------------------------
 
             }
 
-         }
-                
-
-        
+        }
 
         double PositionGraphCalc(double x)
         {
@@ -348,115 +257,56 @@ namespace BluetoothGUISample
             //}
         }
 
+        public delegate void UpdateTextCallback(string message);
 
-        // Set contol mode for the motor
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+
+        private void TestThread()
         {
-            switch (comboBox1.ToString())
+            for (int i = 0; i <= 200000; i++)
             {
-                case "No Control":
-                    controlMode = noControl;
-                    break;
-                case "Manual Motor Speed":
-                    controlMode = openLoop;
-                    break;
-                case "Deadband Testing":
-                    break;
-                case "Positional Control":
-                    controlMode = closedLoop;
-                    break;
-
+                Thread.Sleep(100);
+                TotalTicksBox.Invoke(
+                    new UpdateTextCallback(this.UpdateText),
+                    new object[] { DecTickNew.ToString() }
+                );
             }
         }
 
-
-        // These 3 radio buttons select the PID control mode. Only 1 may be selected at a time. 
-        private void PosRadio_CheckedChanged(object sender, EventArgs e)
+        private void UpdateText(string message)
         {
-            RadioButton rb = sender as RadioButton;
-            if (rb != null)
-            {
-                if (rb.Checked)
-                {
-                    PIDMode = position;
-                }
-            }
+            TotalTicksBox.AppendText(message + "\n");
         }
 
-        private void VelRadio_CheckedChanged(object sender, EventArgs e)
+
+
+
+
+
+
+        /*public void updateTotalTicksBox(string pText)
+         {
+             TotalTicksBox.AppendText(pText);
+
+         }
+
+
+         public void Tb1ControlsUpdate()
+         {
+             var _updater = new Action<string>(updateTotalTicksBox);
+
+             while (true)
+             {
+
+                 this.Invoke(_updater, TotalTicks.ToString());
+                 Thread.Sleep(100);
+
+             }
+         }*/
+
+        /*private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            RadioButton rb = sender as RadioButton;
-            if (rb != null)
-            {
-                if (rb.Checked)
-                {
-                    PIDMode = velocity;
-                }
-            }
-
-        }
-
-        private void AccRadio_CheckedChanged(object sender, EventArgs e)
-        {
-            RadioButton rb = sender as RadioButton;
-            if (rb != null)
-            {
-                if (rb.Checked)
-                {
-                    PIDMode = acceleration;
-                }
-            }
-        }
-
-        private void KPButt_Click(object sender, EventArgs e)
-        {
-            Kp = (float)KdRoll.Value;
-        }
-
-        private void KIButt_Click(object sender, EventArgs e)
-        {
-            Ki = (float)KIRoll.Value;
-        }
-
-        private void KDButt_Click(object sender, EventArgs e)
-        {
-            Kd = (float)KdRoll.Value;
-        }
-
-        private void KPRoll_ValueChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void PosButton_Click_1(object sender, EventArgs e)
-        {
-            if (PosRadio.Checked == true)
-                posSet = (float)NextPos.Value;
-        }
-
-        private void VelButton_Click_1(object sender, EventArgs e)
-        {
-            if (VelRadio.Checked == true)
-                velSet = (float)NextVel.Value;
-        }
-
-        private void AccButton_Click_1(object sender, EventArgs e)
-        {
-            if (AccRadio.Checked == true)
-                accSet = (float)NextAcc.Value;
-        }
-
-        private void OLSpeedSlide_Scroll(object sender, EventArgs e)
-        {
-            OLSpeedRoll.Value = OLSpeedSlide.Value;
-            OLSpeed = (byte)((OLSpeedRoll.Value + 100) / 200 * 255);
-        }
-
-        private void OLSpeedRoll_ValueChanged(object sender, EventArgs e)
-        {
-            OLSpeedSlide.Value = (int)OLSpeedRoll.Value;
-            OLSpeed = (byte)((OLSpeedSlide.Value + 100) / 200 * 255);
-        }
+            thrtb1.Abort();
+        }*/
     }
 
 }
